458e52c837017c08ee80cc302d4ed8b064088b1c
mmitkevich <mikhail.mitkevich@gmail.com>
2014-12-25 17:28:57 +0300

    Kvantor example prices portfolio of options (still dirty vers)

--- Kvant/Calendar.fs
+++ Kvant/Calendar.fs
@@ -1,4 +1,5 @@
-﻿module Kvant.Russia
+﻿module Kvant.Calendar.Russia
+
 open System
 
 let holidays = [
@@ -6,8 +7,11 @@ let holidays = [
         ; (1,5)
     ]
 
-let maturity year month =
+let expirationDate year month =
      match month with
      | 1-> DateTime(year,month,15)
      | 2-> DateTime(year,month,15)
      | _-> DateTime(year,month,16)
+
+
+let maturity (now:DateTime) (expirationDate:DateTime) = expirationDate.Subtract(now).TotalDays / 365.
--- Kvantor/Program.fs
+++ Kvantor/Program.fs
@@ -1,15 +1,50 @@
-﻿open Kvant
+﻿open System
+open Kvant
 
-let rub = Asset.Currency("RUB")
-let usd_rub = Asset.Spot(rub, "USD_RUB", price=60000.)
-let usd_rub_fut = Asset.Future(usd_rub)
+type Position = float*Asset
+let now = DateTime.Now
+let rate = 0.1
+let volatility = 0.5
+let expirations = [|DateTime(2015,1,15); DateTime(2015,3,16)|]
 
-let portfolio = [
-    (1., Asset.VanillaOption(usd_rub, Call, 60000., maturity=0.2, rate=0.1, volatility=0.5));
-    (1., Asset.VanillaOption(usd_rub, Call, 65000., maturity=0.2, rate=0.1, volatility=0.5))
-    (1., Asset.VanillaOption(usd_rub, Call, 65000., maturity=0.2, rate=0.1, volatility=0.5))
-    ]
+let futpos =  15.
+let call und strike iexp now = Asset.VanillaOption(und, Call, strike, maturity = Calendar.Russia.maturity now expirations.[iexp], rate=rate, volatility=volatility)
+let put und strike iexp now = Asset.VanillaOption(und, Put, strike, maturity = Calendar.Russia.maturity now expirations.[iexp], rate=rate, volatility=volatility)
+
+let calc spot = 
+    let rub = Asset.Currency("RUB")
+    let usd_rub = Asset.Spot(rub, "USD_RUB", price=spot)
+    let usd_rub_fut = Asset.Future(usd_rub)
+    let portfolio now = [
+        (5.,    call    usd_rub 60000.     1    now);
+        (10.,   put     usd_rub 60000.     0    now);
+        (10.,   call    usd_rub 70000.     1    now);
+        (12.,   put     usd_rub 67000.     0    now);
+        (4.,    put     usd_rub 70000.     1    now);
+        (10.,   call    usd_rub 60000.     0    now);
+        (5.,    call    usd_rub 65000.     0    now);
+        (1.,    call    usd_rub 55000.     1    now);
+        (-5.,   put     usd_rub 50000.     1    now);
+        (16.,   put     usd_rub 55000.     0    now);
+        (8.,    put     usd_rub 70000.     0    now);
+        ]
+
+    let delta (pf:Position list) = 
+        let total = pf |> List.sumBy (fun pos -> 
+            let (amount,asset) = pos
+            let (price,greeks) = BlackScholes.price asset
+            //printfn "%A * %A\t %A\t %A\t %A\t %A" amount asset.vanillaKind asset.strike asset.maturity.Value price greeks
+            amount*greeks.delta)
+        total
+    
+    delta (portfolio now)
+
 
 [<EntryPoint>]
 let main argv = 
-    0   
+    [45000.; 50000.; 53000.; 55000.; 57000.; 58000.; 59000.; 60000.; 65000.; 70000.; 75000.] |> List.iter(fun s ->
+        let deltaOpt = calc s 
+        printfn "%A \t\t delta (options) = %A\t\t  (futures) = %A\t\t  (total) %A" s deltaOpt futpos (deltaOpt+futpos)
+        )
+    
+    0
